d015b61785d6de20e197676d0c1a363352adc152
Daniel Kiamba <treydancan@gmail.com>
2025-02-22 23:59:00 +0300

    commit additional changes

---
+++ biz.js
@@ -0,0 +1,22 @@
+/*let currentDate = new Date();
+let date1 = document.getElementById(`date`);
+date1.innerHTML = currentDate; */
+function updateDateTime(){
+    let now = new Date();
+    let options = {
+        weekday: `long`,
+        year: `numeric`,
+        month: `long`,
+        day: `numeric`,
+        hour: `2-digit`,
+        minute: `2-digit`,
+        second: `2-digit`,
+        timeZoneName: `short`
+
+
+    };
+    let formatedDateTime = now.toLocaleString(undefined, options);
+    document.getElementById(`date`).innerHTML = formatedDateTime;
+}
+updateDateTime();
+setInterval(updateDateTime, 1000);
